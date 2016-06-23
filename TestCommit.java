81ca225233a65653500cec1700650b743af13461
2003a2003 <2003a2003@ukr.net>
2016-06-23 07:09:19 +0300

    first commit

--- TestCommit.java
+++ TestCommit.java
@@ -1,6 +1,6 @@
 
 public class TestCommit {
     public static void main(String[] args) {
-        System.out.println("First file on Gisthib");
+        System.out.println("First file on Gisthib ^)");
     }
 }

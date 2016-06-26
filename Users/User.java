d251e70343cc6b784a274ff5db0f4d399d4c76f3
2003a2003 <2003a2003@ukr.net>
2016-06-26 09:51:18 +0300

    start create Users

---
+++ Users/User.java
@@ -0,0 +1,11 @@
+package Users;
+
+public class User {
+
+    private int id;
+    private String login;
+    private String passwd;
+    private String firstName;
+    private String secondName;
+
+}

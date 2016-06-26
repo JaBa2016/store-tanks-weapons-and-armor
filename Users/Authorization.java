71a3d201ac2326d0bcb3f81977d7433f9c9ce10a
2003a2003 <2003a2003@ukr.net>
2016-06-26 22:13:38 +0300

    some work

--- Users/Authorization.java
+++ Users/Authorization.java
@@ -8,15 +8,32 @@ public class Authorization {
     private boolean passwd = false;
     private Buffer buffer;
     private List<User> users;
+    private int userID;
 
     public Authorization(String login, String passwd, Buffer buffer) {
         this.buffer = buffer;
         users = buffer.getUsers();
         String passwdMD5 = new Passwd().md5Custom(passwd);
-
-
+        this.login = verifyUser(login);
+        this.passwd = verifyPassword(passwdMD5);
     }
 
+    private boolean verifyUser(String user) {
+        List<User> users = buffer.getUsers();
+        for (User u : users) {
+            if (u.getLogin().equals(user)) {
+                login = true;
+            }
+        }
+        return login;
+    }
+
+    private boolean verifyPassword(String passwdMD5) {
+        if (users.get(userID).getPasswd().equals(passwdMD5)) {
+            passwd = true;
+        }
+        return passwd;
+    }
 
     public boolean isLogin() {
         return login;
@@ -33,6 +50,4 @@ public class Authorization {
     public void setPasswd(boolean passwd) {
         this.passwd = passwd;
     }
-
-
 }
